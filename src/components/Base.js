84bc8d24869c1df6f110409e85f261626201c45c
Femi Balogun <phembarl@yahoo.com>
2024-02-12 10:15:04 +0100

    course complete

--- src/components/Base.js
+++ src/components/Base.js
@@ -1,34 +1,95 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw',
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { type: 'spring', delay: 0.3 },
+  },
+  exit: {
+    x: '-100vw',
+    transition: { ease: 'easeInOut' },
+  },
+};
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: '0px 0px 8px rgb(255, 255, 255)',
+    boxShadow: '0px 0px 8px rgb(255, 255, 255)',
+    transition: {
+      yoyo: Infinity,
+      duration: 0.3,
+    },
+  },
+};
 
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
-  return (
-    <div className="base container">
+  const nextVariants = {
+    hidden: {
+      x: '-100vw',
+    },
+    visible: {
+      x: 0,
+      transition: { type: 'spring', stiffness: 120 },
+    },
+  };
 
+  return (
+    <motion.div
+      className="base container"
+      // initial={{ x: '100vw' }}
+      // animate={{ x: 0 }}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      // transition={{ type: 'spring', delay: 0.3 }}
+      exit="exit"
+    >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
         {bases.map(base => {
           let spanClass = pizza.base === base ? 'active' : '';
           return (
-            <li key={base} onClick={() => addBase(base)}>
-              <span className={spanClass}>{ base }</span>
-            </li>
-          )
+            <motion.li
+              key={base}
+              onClick={() => addBase(base)}
+              whileHover={{ scale: 1.3, originX: 0, color: '#f8e112' }}
+              transition={{ type: 'spring', stiffness: 300 }}
+            >
+              <span className={spanClass}>{base}</span>
+            </motion.li>
+          );
         })}
       </ul>
 
       {pizza.base && (
-        <div className="next">
+        <motion.div
+          className="next"
+          // initial={{ x: '-100vw' }}
+          // animate={{ x: 0 }}
+          // transition={{ type: 'spring', stiffness: 120 }}
+          variants={nextVariants}
+          // initial="hidden"
+          // animate="visible"
+        >
           <Link to="/toppings">
-            <button>Next</button>
+            <motion.button variants={buttonVariants} whileHover="hover">
+              Next
+            </motion.button>
           </Link>
-        </div>
+        </motion.div>
       )}
-
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Base;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import Loader from './Loader';
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: '0px 0px 8px rgb(255, 255, 255)',
+    boxShadow: '0px 0px 8px rgb(255, 255, 255)',
+    transition: {
+      yoyo: Infinity,
+      duration: 0.3,
+    },
+  },
+};
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: { delay: 0.5, duration: 1.5 },
+  },
+  exit: {
+    x: '-100vw',
+    transition: { ease: 'easeInOut' },
+  },
+};
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
+    <motion.div
+      variants={containerVariants}
+      className="home container"
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      transition={{ delay: 0.5, duration: 1.5 }}
+    >
+      <motion.h2 animate={{ fontSize: 50, color: '#ff2994' }}>
         Welcome to Pizza Joint
-      </h2>
+      </motion.h2>
       <Link to="/base">
-        <button>
+        <motion.button variants={buttonVariants} whileHover="hover">
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
-  )
-}
+      <Loader />
+    </motion.div>
+  );
+};
 
 export default Home;
